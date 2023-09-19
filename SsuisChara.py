45994982a09fe34529765865c9a1958cee3b4654
Genglin Guo <108860907+guogenglin@users.noreply.github.com>
2023-09-19 14:46:49 +0800

    Update SsuisChara.py

--- SsuisChara.py
+++ SsuisChara.py
@@ -560,7 +560,7 @@ def generate_heatmap(all_input_names, refpath, all_vfs, mode):
         pathlib.Path(repa).unlink()
     
 def main():
-    print('If you have any quesions or suggestions for SsuisChara, please contact Genglin Guo, e-mail: 2019207025@njau.edu.cn')
+    print('If you have any questions or suggestions for SsuisChara, please contact Genglin Guo, e-mail: 2019207025@njau.edu.cn')
     starttime = time.perf_counter()
     # Initialize
     args = get_argument().parse_args()
